9cb178f597aa50aa5ab809f34f2ec089c572de81
Calum <calumcmccall@gmail.com>
2022-09-12 15:48:13 +0100

    mvp and extensions done

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,31 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import org.w3c.dom.Attr;
+import people.Visitor;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(ArrayList<Attraction> attractions, ArrayList<Stall> stalls) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        ArrayList<IReviewed> reviewed = new ArrayList<>();
+        for (Attraction attraction: attractions) {
+            reviewed.add(attraction);
+        }
+        return reviewed;
+    }
+
+    public void visit(Visitor visitor, Attraction attraction){
+        visitor.visitAttraction(attraction);
+        attraction.setVisitCount(attraction.getVisitCount() + 1);
+    }
 }
